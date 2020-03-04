5ef13bad1c2469240f9863493e6b0fbf2a61db68
Jared Lunde <jared@getadmiral.com>
2020-03-04 07:57:42 -0700

    Add styled-components to template

--- template/config-overrides.js
+++ template/config-overrides.js
@@ -4,6 +4,14 @@ const { override, addWebpackAlias } = require("customize-cra");
 module.exports = override(
   addWebpackAlias({
     react$: path.resolve(__dirname, "node_modules/react"),
-    "react-dom$": path.resolve(__dirname, "node_modules/react-dom")
+    "react-dom$": path.resolve(__dirname, "node_modules/react-dom"),
+    "styled-components$": path.resolve(
+      __dirname,
+      "node_modules/styled-components"
+    ),
+    "styled-components/macro$": path.resolve(
+      __dirname,
+      "node_modules/styled-components/macro"
+    )
   })
 );
